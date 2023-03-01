d0c9d9561b94846b6f22ea754548e0c9b5a5583c
ÄntonPakhorukov <pakhorukov.a@mail.ru>
2023-03-01 23:09:40 +0700

    Частично сделал, не успел допилить со скобками. доделать в свободное время

---
+++ Client.java
@@ -0,0 +1,37 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.Scanner;
+import java.net.UnknownHostException;
+
+public class Client {
+    public static void main(String[] args) throws SocketException {
+        Scanner scanner = new Scanner(System.in);
+        try (Socket socket = new Socket("Localhost", 1234)) {
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            while (true){
+                System.out.println("Введите выражение через пробел: ");
+                String request = scanner.nextLine();
+                if (request.contains(",")) {
+                    System.out.println("Не корректный ввод");
+                    continue;
+                } 
+                if (!(request.contains(" "))) {
+                    System.out.println("Не корректный ввод");
+                    continue;
+                }
+                dataOutputStream.writeUTF(request);
+                if (request.equals("end")) break;
+                System.out.println("Получен ответ от сервера: " + dataInputStream.readUTF());
+            }
+            scanner.close();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ Server.java
@@ -0,0 +1,48 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+
+public class Server {
+    public static void main(String[] args) throws SocketException {
+        try (ServerSocket serverSocket = new ServerSocket(1234)) {
+            System.out.println("Сервер запущен, ожидаем подключение...");
+            Socket socket = serverSocket.accept();
+            System.out.println("Клиент подключился");
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            while(true) {
+                String clientRequest = dataInputStream.readUTF();
+                String[] str = clientRequest.split(" ");
+                double a = Double.parseDouble(str[0]);
+                String sign = str[1];
+                double b = Double.parseDouble(str[2]);
+                double result;
+                switch (sign) {
+                    case "+":
+                        result = a + b;
+                        dataOutputStream.writeUTF(String.format("Сумма чисел равна: %.2f", result));
+                        break;    
+                    case "-":
+                        result = a - b;
+                        dataOutputStream.writeUTF(String.format("Разница чисел равна: %.2f", result));
+                        break;
+                    case "*":
+                        result = a * b;
+                        dataOutputStream.writeUTF(String.format("Умножение чисел равно: %.2f", result));
+                        break;
+                    case "/":
+                        result = a / b;
+                        dataOutputStream.writeUTF(String.format("Умножение чисел равно: %.2f", result));
+                        break;
+                }
+                if (clientRequest.equals("end")) break;
+                System.out.println("Клиент сказал" + clientRequest);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
